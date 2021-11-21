d032a6cb3a2b4ce6eeba546152a0ef30dc277b59
Gerd Reiss <gerd@reiss.pro>
2021-11-21 12:20:54 +0100

    using monocle

--- src/main/scala/learningscala3/rockthejvm/Lenses.scala
+++ src/main/scala/learningscala3/rockthejvm/Lenses.scala
@@ -43,6 +43,47 @@ object Lenses extends App:
   println(s"Initial guitar: $kirksFavGuitar")
   println(s"Updated guitar: $formattedGuitar")
   println(s"Kirk's fav guitar model: $kirksGuitarModel2")
-  println(s"Metallica lensefixed: $metallicaFixed2")
+  println(s"Metallica lensfixed: $metallicaFixed2")
 
-// prisms
+  // prisms
+  sealed trait Shape
+  case class Circle(radius: Double)                    extends Shape
+  case class Rectangle(w: Double, h: Double)           extends Shape
+  case class Triangle(a: Double, b: Double, c: Double) extends Shape
+
+  val circle       = Circle(20)
+  val rectangle    = Rectangle(10, 20)
+  val triangle     = Triangle(3, 4, 5)
+  val shape: Shape = circle
+
+  // increase size without prisms
+  val newShape = shape match
+    case Circle(r)         => Circle(r * 2)
+    case Rectangle(w, h)   => Rectangle(w * 2, h)
+    case Triangle(a, b, c) => Triangle(a + 1, b + 1, c + 1)
+
+  import monocle.Prism
+
+  val circlePrism = Prism[Shape, Double] {
+    case Circle(r) => Some(r)
+    case _         => None
+  }(Circle.apply)
+
+  val circle2  = circlePrism(40)                 // Circle(40)
+  val noRadius = circlePrism.getOption(triangle) // None
+  val radius   = circlePrism.getOption(circle2)  // Some(40)
+
+  case class Icon(background: String, shape: Shape)
+  case class Logo(color: String)
+  case class BrandIdentity(logo: Logo, icon: Icon)
+
+  val brandIdentityIconLens             = GenLens[BrandIdentity](_.icon)
+  val iconShapeLens                     = GenLens[Icon](_.shape)
+  val brandIdentityIconCircleRadiusLens =
+    brandIdentityIconLens.andThen(iconShapeLens).andThen(circlePrism)
+
+  val brand    = BrandIdentity(Logo("red"), Icon("white", Circle(40)))
+  val modified = brandIdentityIconCircleRadiusLens.modify(_ + 10)(brand)
+
+  println(s"Original brand: $brand")
+  println(s"Modified brand: $modified")
