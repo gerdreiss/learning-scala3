fba62916fcf6dd6d0c4f931424635cfc514a1b02
Gerd Reiss <gerd@reiss.pro>
2021-12-16 10:30:35 +0100

    Coursera Effective Programming in Scala 3

---
+++ src/main/scala/learningscala3/coursera/effectiveprogramminginscala/cardgame.scala
@@ -0,0 +1,51 @@
+package learningscala3.coursera.effectiveprogramminginscala
+
+enum Shape:
+  case Diamond, Squiggle, Oval
+
+enum Number:
+  case One, Two, Three
+
+enum Shading:
+  case Solid, Striped, Open
+
+enum Color:
+  case Red, Green, Purple
+
+case class Card(shape: Shape, number: Number, shading: Shading, color: Color)
+
+val deck = (
+  Card(Shape.Oval, Number.One, Shading.Open, Color.Red),
+  Card(Shape.Diamond, Number.Two, Shading.Solid, Color.Green),
+  Card(Shape.Squiggle, Number.Three, Shading.Striped, Color.Purple)
+)
+
+// Three cards make up a set if all the following properties are satisfied.
+// 1. The three cards all have the same number of shapes, or they have three different numbers of shapes.
+// 2. The three cards all have the same shape or they have three different shapes.
+// 3. Then three cards all have the same shading, or they have three different shadings,
+// 4. The three cards all have the same color or they have three different colors.
+
+def checkShapes(deck: (Card, Card, Card)): Boolean =
+  validSet(Set(deck._1.shape, deck._2.shape, deck._3.shape))
+
+def checkNumbers(deck: (Card, Card, Card)): Boolean =
+  validSet(Set(deck._1.number, deck._2.number, deck._3.number))
+
+def checkShadings(deck: (Card, Card, Card)): Boolean =
+  validSet(Set(deck._1.shading, deck._2.shading, deck._3.shading))
+
+def checkColors(deck: (Card, Card, Card)): Boolean =
+  validSet(Set(deck._1.color, deck._2.color, deck._3.color))
+
+def validSet[A](set: Set[A]): Boolean =
+  set.size == 1 || set.size == 3
+
+def isValidSet(deck: (Card, Card, Card)): Boolean =
+  checkShapes(deck) &&
+    checkNumbers(deck) &&
+    checkShadings(deck) &&
+    checkColors(deck)
+
+@main def cards(): Unit =
+  println(isValidSet(deck))
---
+++ src/main/scala/learningscala3/coursera/effectiveprogramminginscala/footprint.scala
@@ -0,0 +1,34 @@
+package learningscala3.coursera.effectiveprogramminginscala
+
+case class Experience(duration: Int, definition: Double, network: Network)
+
+enum Network:
+  case Fixed, Mobile
+
+val lowQuality  = 0.3 // MB/s
+val highQuality = 0.6 // MB/s
+
+val thirtyMinutes = 30 * 60 // seconds
+
+val highQualityAndMobile =
+  Experience(thirtyMinutes, highQuality, Network.Mobile)
+
+val lowQualityAndFixed =
+  Experience(thirtyMinutes, lowQuality, Network.Fixed)
+
+val dataCenterEnergy = 0.000072
+val kgCO2PerKwh      = 0.5
+
+def networkEnergy(network: Network): Double =
+  network match
+    case Network.Fixed  => 0.00043
+    case Network.Mobile => 0.00088
+
+def footprint(experience: Experience): Double =
+  val megabytes = experience.duration * experience.definition
+  val energy    = dataCenterEnergy + networkEnergy(experience.network)
+  energy * megabytes * kgCO2PerKwh
+
+@main def footprint(): Unit =
+  println(s"High quality mobile experience expenditure      : ${footprint(highQualityAndMobile)}")
+  println(s"Low quality fixed network experience expenditure: ${footprint(lowQualityAndFixed)}")
