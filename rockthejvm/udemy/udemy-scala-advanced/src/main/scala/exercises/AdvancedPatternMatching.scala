1618b865ce3622fd1a915f13c0e291a327ec8d89
Gerd Reiss <gerd@reiss.pro>
2022-01-01 11:57:36 +0100

    Rock the JVM: Advanced Scala and FP - advanced pattern matching part 1 + exercise

---
+++ rockthejvm/udemy/udemy-scala-advanced/src/main/scala/exercises/AdvancedPatternMatching.scala
@@ -0,0 +1,56 @@
+package exercises
+
+object AdvancedPatternMatching extends App:
+
+  val n: Int = 4
+
+  // solve this via pattern matching
+  val matchedProperty = n match
+    case x if x < 10 => "single digit"
+    case x if x % 2 == 0 => "an even number"
+    case _ => "no match"
+
+  println(matchedProperty)
+
+  object SingleDigit:
+    def unapply(n: Int): Option[Int] =
+      if n < 10 then Some(n) else None
+
+  object EvenNumber:
+    def unapply(n: Int): Option[Int] =
+      if n % 2 == 0 then Some(n) else None
+
+  val matchedProperty2 = n match
+    case SingleDigit(x) => "single digit"
+    case EvenNumber(x) => "an even number"
+    case _ => "no match"
+
+  println(matchedProperty2)
+
+  object Property:
+    def unapply(n: Int): Option[String] =
+      if n < 10 then Some("single digit")
+      else if n % 2 == 0 then Some("an even number")
+      else None
+
+  val matchedProperty3 = n match
+    case Property(p) => p
+    case _ => "no match"
+
+  println(matchedProperty3)
+
+  // Daniel's solution:
+  object even:
+    def unapply(n: Int): Boolean =
+      n % 2 == 0
+
+  object singleD:
+    def unapply(n: Int): Boolean =
+      n > -10 && n < 10
+
+  val danielled = n match
+    case singleD() => "single digit"
+    case even() => "an even number"
+    case _ => "no match"
+
+  println(danielled)
--- rockthejvm/udemy/udemy-scala-advanced/src/main/scala/lectures/Section2_Lecture6/AdvancedPatternMatching.scala
+++ rockthejvm/udemy/udemy-scala-advanced/src/main/scala/lectures/Section2_Lecture6/AdvancedPatternMatching.scala
@@ -2,4 +2,34 @@ package lectures.Section2_Lecture6
 
 object AdvancedPatternMatching extends App:
 
-  println("Here we go!")
+  val numbers = List(1)
+  val description = numbers match
+    case head :: Nil => s"the only element is $head"
+    case _ => "unclear situation"
+
+  println(description)
+
+  /** Pattern matchable things are:
+    *   - constants
+    *   - wildcards
+    *   - case classes
+    *   - tuples
+    *   - some special magic like above
+    */
+  class Person(val name: String, val age: Int)
+  object Person:
+    def unapply(p: Person): Option[(String, Int)] = Some((p.name, p.age))
+    // and now for something crazy
+    def unapply(age: Int): Option[String] =
+      Some(if age < 21 then "minor" else "major")
+
+  val bob = Person("Bob", 42)
+  val greeting = bob match
+    case Person(n, a) => s"Name: $n, age: $a"
+
+  println(greeting)
+
+  val legalStatus = bob.age match
+    case Person(status) => s"Legal status: $status"
+
+  println(legalStatus)
