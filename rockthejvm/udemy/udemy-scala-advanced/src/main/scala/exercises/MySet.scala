2dfa5a22d0cf9465a11c605ef414e0b974c68e7c
Gerd Reiss <gerd@reiss.pro>
2022-01-02 12:12:39 +0100

    Rock the JVM: Advanced Scala and FP - potentially infinite set

--- rockthejvm/udemy/udemy-scala-advanced/src/main/scala/exercises/MySet.scala
+++ rockthejvm/udemy/udemy-scala-advanced/src/main/scala/exercises/MySet.scala
@@ -1,5 +1,7 @@
 package exercises
 
+import scala.sys.Prop
+
 trait MySet[A] extends (A => Boolean):
   def apply(a: A): Boolean = contains(a)
 
@@ -14,7 +16,7 @@ trait MySet[A] extends (A => Boolean):
   def filter(p: A => Boolean): MySet[A]
   def filterNot(p: A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
-  def unary_! : MySet[A] // my solution = filterNot(this)
+  def unary_! : MySet[A]
 
 end MySet
 
@@ -30,10 +32,11 @@ class EmptySet[A]() extends MySet[A]:
   override def filter(p: A => Boolean): MySet[A] = this
   override def filterNot(p: A => Boolean): MySet[A] = this
   override def foreach(f: A => Unit): Unit = ()
-  override def unary_! : MySet[A] = AllInclusiveSet()
+  override def unary_! : MySet[A] = PropertyBasedSet(_ => true)
 
 end EmptySet
 
+// this one is useless
 class AllInclusiveSet[A] extends MySet[A]:
   def contains(a: A): Boolean = true
   def +(a: A): MySet[A] = this
@@ -44,25 +47,27 @@ class AllInclusiveSet[A] extends MySet[A]:
   def map[B](f: A => B): MySet[B] = ??? // impossible to implement
   def flatMap[B](f: A => MySet[B]): MySet[B] = ??? // impossible to implement
   def filter(p: A => Boolean): MySet[A] = ??? // property-based set
-  def filterNot(p: A => Boolean): MySet[A] = ??? // impossible to implement
+  def filterNot(p: A => Boolean): MySet[A] = ??? // property-based set?
   def foreach(f: A => Unit): Unit = ??? // impossible to implement
   def unary_! : MySet[A] = EmptySet()
 
 end AllInclusiveSet
 
 class PropertyBasedSet[A](property: A => Boolean) extends MySet[A]:
-  def contains(a: A): Boolean = ???
-  def +(a: A): MySet[A] = ???
-  def ++(that: MySet[A]): MySet[A] = ???
-  def -(a: A): MySet[A] = ???
-  def --(that: MySet[A]): MySet[A] = ???
-  def &(that: MySet[A]): MySet[A] = ???
-  def map[B](f: A => B): MySet[B] = ???
-  def flatMap[B](f: A => MySet[B]): MySet[B] = ???
-  def filter(p: A => Boolean): MySet[A] = ???
-  def filterNot(p: A => Boolean): MySet[A] = ???
-  def foreach(f: A => Unit): Unit = ???
-  def unary_! : MySet[A] = ???
+  def contains(a: A): Boolean = property(a)
+  def +(a: A): MySet[A] = PropertyBasedSet(x => property(x) || x == a)
+  def ++(that: MySet[A]): MySet[A] = PropertyBasedSet(x => property(x) || that.contains(x))
+  def -(a: A): MySet[A] = filter(_ != a)
+  def --(that: MySet[A]): MySet[A] = filter(!that)
+  def &(that: MySet[A]): MySet[A] = filter(that)
+  def map[B](f: A => B): MySet[B] = politelyFail
+  def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
+  def filter(p: A => Boolean): MySet[A] = PropertyBasedSet(x => property(x) && p(x))
+  def filterNot(p: A => Boolean): MySet[A] = PropertyBasedSet(x => property(x) && !p(x))
+  def foreach(f: A => Unit): Unit = politelyFail
+  def unary_! : MySet[A] = PropertyBasedSet(x => !property(x))
+
+  private def politelyFail = throw java.lang.IllegalArgumentException("Really deep rabbit hole!")
 
 end PropertyBasedSet
 
@@ -124,7 +129,7 @@ class NonEmptySet[A](val head: A, val tail: MySet[A]) extends MySet[A]:
     f(head)
     tail.foreach(f)
 
-  override def unary_! : MySet[A] = this
+  override def unary_! : MySet[A] = PropertyBasedSet(x => !this.contains(x))
 
 end NonEmptySet
 
@@ -139,6 +144,18 @@ object MySet:
 
 @main def testMySet(): Unit =
   val s = MySet(1, 2, 3, 4)
-  s + 5 ++ MySet(-1, -2) + 3 map (_ * 10) flatMap (x =>
-    MySet(x, x + 1)
-  ) filter (_ % 2 == 0) foreach println
+  (s + 5 ++ MySet(-1, -2) + 3)
+    .map(_ * 10)
+    .flatMap(x => MySet(x, x + 1))
+    .filter(_ % 2 == 0)
+    .foreach(println)
+
+  val notS = !s // all the naturals not equal to 1,2,3,4
+  println(notS(2)) // false
+  println(notS(5)) // true
+
+  val evenNotS = notS.filter(_ % 2 == 0)
+  println(evenNotS(5)) // false
+
+  val `evenNotS + 5` = evenNotS + 5
+  println(`evenNotS + 5`(5)) // true
