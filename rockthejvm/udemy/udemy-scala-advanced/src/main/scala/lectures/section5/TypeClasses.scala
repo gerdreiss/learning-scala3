5d6ad35fc66b2a45a53ef83bae6926f137e08d31
Gerd Reiss <gerd@reiss.pro>
2022-01-10 07:36:43 +0100

    Rock the JVM: Advanced Scala and FP - type classes

---
+++ rockthejvm/udemy/udemy-scala-advanced/src/main/scala/lectures/section5/TypeClasses.scala
@@ -0,0 +1,18 @@
+package lectures.section5
+
+case class Person(name: String, age: Int)
+
+trait HtmlSerializer[T]:
+  def serialize(value: T): String
+
+object HtmlSerializer:
+  def serialize[T](value: T)(using serializer: HtmlSerializer[T]): String =
+    serializer.serialize(value)
+
+  def apply[T](using serializer: HtmlSerializer[T]) = serializer
+
+given HtmlSerializer[Person] with
+  def serialize(p: Person): String = s"<p>Name: ${p.name}, age: ${p.age}</p>"
+
+@main def testTypeClass(): Unit =
+  println(HtmlSerializer.serialize(Person("Joe", 40)))
