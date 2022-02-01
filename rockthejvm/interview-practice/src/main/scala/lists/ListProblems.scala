902052676586e92bd61e00e391cdb1d8567b9656
Gerd Reiss <gerd@reiss.pro>
2022-02-01 18:31:49 +0100

    Rock the JVM: Interview Practice - Reverse + Append

--- rockthejvm/interview-practice/src/main/scala/lists/ListProblems.scala
+++ rockthejvm/interview-practice/src/main/scala/lists/ListProblems.scala
@@ -9,7 +9,9 @@ sealed abstract class RList[+T]: // our covariant list
   def headOption: Option[T]
   def head: T
   def tail: RList[T]
-
+  def length: Int
+  def reverse: RList[T]
+  def :+[S >: T](elem: S): RList[S]
   def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
 
 end RList
@@ -20,6 +22,9 @@ case object RNil extends RList[Nothing]:
   override def headOption: Option[Nothing] = None
   override def head: Nothing               = throw ju.NoSuchElementException()
   override def tail: RList[Nothing]        = throw ju.NoSuchElementException()
+  override def length: Int                 = 0
+  def :+[S >: Nothing](elem: S): RList[S]  = elem :: this
+  override def reverse: RList[Nothing]     = this
 
   override def toString: String = "[]"
 
@@ -34,6 +39,35 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
   override def isEmpty: Boolean = false
 
   override def headOption: Option[T] = Some(head)
+
+  override def length: Int =
+    // stack overflow danger here:
+    // 1 + tail.length
+    // therefore a tail recursive function:
+    @tailrec
+    def lengthTailrec(remaining: RList[T], acc: Int): Int =
+      if remaining.isEmpty then acc
+      else lengthTailrec(remaining.tail, acc + 1)
+
+    lengthTailrec(this, 0)
+
+  override def :+[S >: T](elem: S): RList[S] =
+    if tail.isEmpty then head :: elem :: tail
+    else head :: (tail :+ elem)
+
+  override def reverse: RList[T] =
+    // this doesn't work - obviously, if you think about it...
+    @tailrec
+    def reverseTailrecG(remaining: RList[T], acc: RList[T]): RList[T] =
+      if remaining.isEmpty then acc
+      else reverseTailrecG(remaining.tail, acc :+ remaining.head)
+
+    @tailrec
+    def reverseTailrecDan(remaining: RList[T], acc: RList[T]): RList[T] =
+      if remaining.isEmpty then acc
+      else reverseTailrecDan(remaining.tail, remaining.head :: acc)
+
+    reverseTailrecDan(this, RNil)
 
   override def toString: String =
     @tailrec
@@ -52,14 +86,29 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
 
 end ::
 
+object RList:
+  def from[T](iterable: Iterable[T]): RList[T] =
+    @tailrec
+    def fromTailrec(remaining: Iterable[T], acc: RList[T]): RList[T] =
+      if remaining.isEmpty then acc
+      else fromTailrec(remaining.drop(1), remaining.head :: acc)
+
+    fromTailrec(iterable, RNil).reverse
+
 object ListProblems extends App:
+
+  val largeList = RList.from((1 to 10000))
+
   println(RNil)
   println(::(1, ::(2, ::(3, RNil))))
   println("a" :: "b" :: "c" :: RNil)
 
-  val l = "a" :: "b" :: "c" :: RNil
-  println(l(0))
-  println(l(2))
+  println(largeList(0))
+  println(largeList(2))
   // println(l(3))  // IndexOutOfBoundsException
   // println(l(-1)) // IndexOutOfBoundsException
-  println(RNil(0)) // NoSuchElementException
+  // println(RNil(0)) // NoSuchElementException
+  println(largeList.length)
+  println(RNil.length)
+  println(largeList.reverse)
+  println(largeList.reverse :+ "d")
