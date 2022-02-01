42a1090f06e8716ca920b68d57e2b321ac447063
Gerd Reiss <gerd@reiss.pro>
2022-02-01 17:30:42 +0100

    Rock the JVM: Interview Practice - Get K-th element

--- rockthejvm/interview-practice/src/main/scala/lists/ListProblems.scala
+++ rockthejvm/interview-practice/src/main/scala/lists/ListProblems.scala
@@ -4,22 +4,35 @@ import java.util as ju
 import scala.annotation.tailrec
 
 sealed abstract class RList[+T]: // our covariant list
-  def head: T
-  def tail: RList[T]
+  def apply(index: Int): T
   def isEmpty: Boolean
   def headOption: Option[T]
+  def head: T
+  def tail: RList[T]
+
   def ::[S >: T](elem: S): RList[S] = new ::(elem, this)
 
+end RList
+
 case object RNil extends RList[Nothing]:
-  override def head: Nothing               = throw ju.NoSuchElementException()
-  override def tail: RList[Nothing]        = throw ju.NoSuchElementException()
+  override def apply(index: Int): Nothing  = throw ju.NoSuchElementException()
   override def isEmpty: Boolean            = true
   override def headOption: Option[Nothing] = None
+  override def head: Nothing               = throw ju.NoSuchElementException()
+  override def tail: RList[Nothing]        = throw ju.NoSuchElementException()
 
   override def toString: String = "[]"
 
+end RNil
+
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T]:
-  override def isEmpty: Boolean      = false
+  override def apply(index: Int): T =
+    if index == 0 then head
+    else if index < 0 || tail.isEmpty then throw java.lang.IndexOutOfBoundsException()
+    else tail(index - 1)
+
+  override def isEmpty: Boolean = false
+
   override def headOption: Option[T] = Some(head)
 
   override def toString: String =
@@ -37,7 +50,16 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
 
     s"[${toStringTailrec(this, "")}]"
 
+end ::
+
 object ListProblems extends App:
   println(RNil)
   println(::(1, ::(2, ::(3, RNil))))
   println("a" :: "b" :: "c" :: RNil)
+
+  val l = "a" :: "b" :: "c" :: RNil
+  println(l(0))
+  println(l(2))
+  // println(l(3))  // IndexOutOfBoundsException
+  // println(l(-1)) // IndexOutOfBoundsException
+  println(RNil(0)) // NoSuchElementException
