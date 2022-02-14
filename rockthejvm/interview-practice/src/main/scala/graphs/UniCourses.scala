3d48e143d27a4aa273b752cd3c8582ece17fb511
Gerd Reiss <gerd@reiss.pro>
2022-02-14 16:34:16 +0100

    [Rock the JVM] Interview Practice - Graph problems: university courses 2

--- rockthejvm/interview-practice/src/main/scala/graphs/UniCourses.scala
+++ rockthejvm/interview-practice/src/main/scala/graphs/UniCourses.scala
@@ -20,6 +20,45 @@ def canTakeAllCoursesDan(nCourses: Int, prereqs: List[(Int, Int)]): Boolean =
 
   (0 until nCourses).forall(course => graph.findCycle(course).isEmpty)
 
+end canTakeAllCoursesDan
+
+def findOrderG(n: Int, prereqs: List[(Int, Int)]): List[Int] =
+  val allCourses = (0 until n).map((_, Set.empty[Int])).toMap
+  val allPrereqs = prereqs.toSet.groupMap(_._1)(_._2)
+  val graph = Graph.from(allCourses ++ allPrereqs)
+
+  graph.findPath(0, n)
+
+// topological sort
+def findOrderDan(n: Int, prereqs: List[(Int, Int)]): List[Int] =
+  val dependencies =
+    (0 until n).map((_, Set.empty[Int])).toMap ++
+      prereqs.foldLeft(Map.empty[Int, Set[Int]]) { case (acc, (a, b)) =>
+        acc + (b -> (acc.getOrElse(b, Set.empty) + a))
+      }
+
+  def tailrec(
+      remaining: Set[Int],
+      stack: List[Int] = List.empty,
+      expanding: Set[Int] = Set.empty,
+      expanded: Set[Int] = Set.empty,
+      order: List[Int] = List.empty
+  ): List[Int] =
+    if stack.isEmpty then
+      if remaining.isEmpty then order
+      else tailrec(remaining.tail, List(remaining.head), Set.empty, expanded, order)
+    else
+      val course = stack.head
+      if expanded.contains(course) then tailrec(remaining, stack.tail, expanding, expanded, order)
+      else if expanding.contains(course) then
+        tailrec(remaining, stack.tail, expanding - course, expanded + course, course :: order)
+      else
+        val afters = dependencies(course)
+        if expanding.intersect(afters).nonEmpty then List.empty
+        else tailrec(remaining, afters.toList ++ stack, expanding + course, expanded, order)
+
+  tailrec(dependencies.keySet)
+
 object UniCourses extends App:
 
   println(canTakeAllCoursesDan(2, List((0, 1))))
